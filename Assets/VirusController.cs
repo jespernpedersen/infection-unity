1b60bdca014fc1b061e771abd10e5412fb48f6b0
Jesper Pedersen <jeppenp12@gmail.com>
2021-03-15 14:26:06 +0100

    Add Mutations + public list instead of GameObject

--- Assets/VirusController.cs
+++ Assets/VirusController.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VirusProperties : MonoBehaviour
+public class VirusController : MonoBehaviour
 {
     public float points;
     public float growth;
     public float infectCount;
-    public GameObject[] VirusMutations; 
+    public List<Mutation> MutationsList = new List<Mutation>(0); 
 
     // Start is called before the first frame update
     void Start()
     {    
-        VirusMutations = GameObject.FindGameObjectsWithTag("mutation");
+        MutationsList.Add(new Mutation(0, "My First Mutation", "This evolves", 2));
     }
 
     // Update is called once per frame
